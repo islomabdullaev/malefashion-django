7355ed93e0332b17a7f7c2048be45f3b3a2d9cad
Islom Abdullaev <islom.abdullaev.02@mail.ru>
2023-07-12 19:53:54 +0500

    blogs section

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogTemplateView
+from .views import BlogTemplateView, BlogDetailView
 
 app_name = 'blogs'
 
 urlpatterns = [
-    path('', BlogTemplateView.as_view(), name='list')
+    path('', BlogTemplateView.as_view(), name='list'),
+    path('detail/<int:pk>', BlogDetailView.as_view(), name='detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from blogs.models import PostModel
 # Create your views here.
@@ -8,3 +8,9 @@ from blogs.models import PostModel
 class BlogTemplateView(ListView):
     template_name = 'blog.html'
     model = PostModel
+
+
+class BlogDetailView(DetailView):
+    template_name = 'blog-details.html'
+    model = PostModel
+    context_object_name = 'post'
